4c5c72dfa7795b635a90cab8c628785618673d3a
Ronin <rxswyers@gmail.com>
2018-08-13 11:25:11 -0400

    Typings added
    This should add intellisense for VSC, not sure if it'll work with other text editors. You may have to do some extra configuration in others.
    resolves #2

---
+++ typings/index.d.ts
@@ -0,0 +1,73 @@
+//List all the functions
+declare class NekoClient {
+  getSFWTickle():any;
+  getSFWSlap():any;
+  getSFWPoke():any;
+  getSFWPat():any;
+  getSFWNeko():any;
+  getSFWNekoGif():any;
+  getSFWMeow():any;
+  getSFWLizard():any;
+  getSFWKiss():any;
+  getSFWHug():any;
+  getSFWFoxGirl():any;
+  getSFWFeed():any;
+  getSFWCuddle():any;
+  getSFWWhy():any;
+  getSFWCatText():any;
+  getSFWOwOify(opts: NekoClient.NekoQueryParams):any;
+  getSFWChat(opts: NekoClient.NekoQueryParams):any;
+  getSFW8Ball():any;
+  getSFWFact():any;
+  getSFWKemonomimi():any;
+  getSFWHolo():any;
+  getNSFWRandomHentaiGif():any;
+  getNSFWPussy():any;
+  getNSFWNekoGif():any;
+  getNSFWNeko():any;
+  getNSFWLesbian():any;
+  getNSFWKuni():any;
+  getNSFWCumsluts():any;
+  getNSFWClassic():any;
+  getNSFWBoobs():any;
+  getNSFWBJ():any;
+  getNSFWAnal():any;
+  getNSFWAnalArts():any;
+  getNSFWYuri():any;
+  getNSFWTrap():any;
+  getNSFWTits():any;
+  getNSFWGirlSoloGif():any;
+  getNSFWGirlSolo():any;
+  getNSFWSmallBoobs():any;
+  getNSFWPussyWankGif():any;
+  getNSFWPussyArt():any;
+  getNSFWKemonomimi():any;
+  getNSFWKitsune():any;
+  getNSFWKeta():any;
+  getNSFWHolo():any;
+  getNSFWHoloEro():any;
+  getNSFWHentai():any;
+  getNSFWFutanari():any;
+  getNSFWFemdom():any;
+  getNSFWFeetGif():any;
+  getNSFWEroFeet():any;
+  getNSFWFeet():any;
+  getNSFWEro():any;
+  getNSFWEroKitsune():any;
+  getNSFWEroKemonomimi():any;
+  getNSFWEroNeko():any;
+  getNSFWEroYuri():any;
+  getNSFWCumArts():any;
+  getNSFWBlowJob():any;
+  getNSFWPussyGif():any;
+}
+
+export = NekoClient;
+
+
+declare namespace NekoClient {
+  //Help create options interface for the few functions that need it
+  export interface NekoQueryParams {
+    text?: string;
+  }
+}
